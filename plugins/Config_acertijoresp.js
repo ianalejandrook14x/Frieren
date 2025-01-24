3093eda42bc0eff9376f5b92d79caed1562966e5
Ian.xyz <taiwam34@gmail.com>
2025-01-24 15:46:33 -0300

    PLG

--- plugins/Config_acertijoresp.js
+++ plugins/Config_acertijoresp.js
@@ -5,7 +5,7 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✦ Ese acertijo ya ha terminado!');
+  if (!(id in this.tekateki)) return m.reply('☁ El acertijo ya finalizo');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
@@ -13,8 +13,8 @@ handler.before = async function(m) {
       m.reply(`✦ *Respuesta correcta!*\n+${this.tekateki[id][2]} Centavos`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
-    else m.reply('Respuesta incorrecta!');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras`);
+    else m.reply('Respuesta incorrecta');
   }
   return !0;
 };
