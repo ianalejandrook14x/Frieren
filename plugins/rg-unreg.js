584312d6be2192dc24cb5e2ddd2d11ebf325e8f5
Ian.xyz <taiwam34@gmail.com>
2025-01-24 20:56:31 -0300

    PLG

---
+++ plugins/rg-unreg.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, text }) => {
+
+let user = global.db.data.users[m.sender]
+
+user.registered = false
+return conn.reply(m.chat, `*Se elimino el registro en la base de datos 🌼*`, m, rcanal)
+
+}
+handler.help = ['unreg']
+handler.tags = ['rg']
+handler.command = ['unreg']
+handler.register = true
+export default handler
