9938d4042b539bbc435250a9acdd895a04a96df1
Ian.xyz <taiwam34@gmail.com>
2025-01-24 16:28:41 -0300

    PLG

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -4,17 +4,12 @@ handler.all = async function(m) {
 
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
-if (/^No Te perdono$/i.test(m.text)) {
-conn.reply(m.chat, `Esta bien, agradezco tu sinceridad por haberme respondido, te deseo suerte.`, m, )
+if (/^bot$/i.test(m.text)) {
+conn.reply(m.chat, `Hola, 👋🏻 para ver los comandos del bot utiliza \n.menu`, m, )
 }
 
-if (chat.isBaneed) return
-if (/^reglas$/i.test(m.text)) {
-conn.reply(m.chat, `*Hola si quieres unir a la bot deberas seguir las reglas basicas* 👇\n\n> No hacer spam a la bot\n\n> No llamar a la bot\n\n> El grupo debe de tener un minimo de 25 miembros o no sera otorgado el bot\n\n> No añadir a la bot sin permisos del creador \n\n> Wa.me/5493876639332\n\n> Disfrute de *${botname}Bot-MD Powered By Ianalejandrook15x*`, m, )
-}
-  
-if (/^Si Te perdono$/i.test(m.text)) {
-conn.reply(m.chat, `Muchas gracias agradezco tu sinceridad por perdonarme, juro que dare todo de mi. Te quiero mucho... 😙`, m, )
+if (/^Hola bot$/i.test(m.text)) {
+conn.reply(m.chat, `Hola 👋🏻 soy ${botname}\nutiliza .menu\nPara ver mis comandos`, m, )
 }
 return !0;
 };
