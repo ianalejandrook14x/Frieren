47fe07276d452e8c72a56ac007976375fc5e5c79
Ian.xyz <taiwam34@gmail.com>
2025-01-24 20:40:59 -0300

    PLG

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   await conn.fetchBlocklist().then(async (data) => {
     let txt = `*Lista de usuarios bloqueados*\n\n*Total :* ${data.length}\n\n`;
     for (const i of data) {
-      txt += `✦ @${i.split('@')[0]}\n`;
+      txt += `✨ @${i.split('@')[0]}\n`;
     }
     txt += '';
     return conn.reply(m.chat, txt, m, {mentions: await conn.parseMention(txt)});
