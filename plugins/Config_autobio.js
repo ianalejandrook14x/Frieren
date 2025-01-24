0abab76bbe9f0c6b2a8d95f2c6f951b756e4b239
Ian.xyz <taiwam34@gmail.com>
2025-01-24 15:49:17 -0300

    PLG

--- plugins/Config_autobio.js
+++ plugins/Config_autobio.js
@@ -10,7 +10,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `『${global.botname}』 |「✧」Activo: ${uptime} |「</>」 Developed: ianalejandrook15x` 
+let bio = `${global.botname} 🌼 |「✨」Activo: ${uptime} | Dev: AxelOfc` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } }
