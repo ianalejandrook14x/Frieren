e66faee0e3b4361314961aa51b4bb0982dcbd9c9
Ian.xyz <taiwam34@gmail.com>
2025-01-24 16:25:10 -0300

    PLG

--- plugins/Grupo•tagall.js
+++ plugins/Grupo•tagall.js
@@ -5,8 +5,8 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     return;
   }
   const pesan = args.join` `;
-  const colombia = `✦ *Mensaje:* ${pesan}`;
-  let teks = `✦ *invocando grupo*\n${colombia}\n\n✨ *Tags:*\n`;
+  const colombia = `🌼 *Mensaje:* ${pesan}`;
+  let teks = `☁ *invocando grupo*\n${colombia}\n\n✨ *Tags:*\n`;
   for (const mem of participants) {
     teks += `@${mem.id.split('@')[0]}\n`;
   }
