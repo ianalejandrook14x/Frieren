f40795189d18bb5779ceaa0abdbdfaec1064ef9b
Ian.xyz <taiwam34@gmail.com>
2025-01-24 17:44:41 -0300

    PLG

--- plugins/Rpg•retirar.js
+++ plugins/Rpg•retirar.js
@@ -10,13 +10,13 @@ user.yenes += count * 1
 await m.reply(`✦ Retiraste *${count} ✨ ${currency}* del Banco.`)
 return !0
 }
-if (!Number(args[0])) return m.reply('✦ La cantidad debe ser un Numero.')
+if (!Number(args[0])) return m.reply('✨ La cantidad debe ser un Numero.')
 let count = parseInt(args[0])
 if (!user.bank) return m.reply('No tienes *${currency}* en el Banco.')
 if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ${currency}* en el Banco.`)
 user.bank -= count * 1
 user.yenes += count * 1
-await m.reply(`✦ Retiraste *${count} ${currency}* del Banco.`)}
+await m.reply(`🌼 Retiraste *${count} ${currency}* del Banco.`)}
 
 handler.help = ['retirar']
 handler.tags = ['rpg']
