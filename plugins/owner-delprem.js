509292029ecd82e56d1b0c2d324806fffa625dd2
Ian.xyz <taiwam34@gmail.com>
2025-01-24 20:51:12 -0300

    PLG

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -3,14 +3,14 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   const user = global.db.data.users[who];
-  if (user.premiumTime = 0) throw '*✦ El usuario mencionado no tiene premium*';
+  if (user.premiumTime = 0) throw '*🌼 El usuario mencionado no tiene premium*';
   const txt = text.replace('@' + who.split`@`[0], '').trim();
 
   user.premiumTime = 0;
 
   user.premium = false;
 
-  const textdelprem = `*@${who.split`@`[0]} Ya no es usuario premium ✦*`;
+  const textdelprem = `*@${who.split`@`[0]} Ya no es usuario premium ✨*`;
   m.reply(textdelprem, null, {mentions: conn.parseMention(textdelprem)});
 };
 handler.help = ['delprem <@user>'];
