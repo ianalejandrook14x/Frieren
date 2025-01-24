f95c0d01ec5bfeb510884860e481126e648838aa
Ian.xyz <taiwam34@gmail.com>
2025-01-24 14:55:51 -0300

    _>

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
 import chalk from 'chalk'
-console.log('\n✰ Iniciando ${botname} ✰')
+console.log('\nIniciando Frieren Bot')
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
 const { name, description, collaborators, author, version } = require(join(__dirname, './package.json'))
@@ -23,7 +23,7 @@ if (collaborators.hasOwnProperty(key)) {
 activeCollaborators += collaborators[key] + ', '
 }}
 activeCollaborators = activeCollaborators.slice(0, -2);
-cfonts.say('Bot\nBy Ian', {
+cfonts.say('Frieren\AI', {
 align: 'center',           
 gradient: ['red', 'blue'] 
 })
