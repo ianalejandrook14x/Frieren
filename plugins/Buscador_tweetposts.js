ddb00a1ebd09e4670cd80372c546db92205dd56f
Ian.xyz <taiwam34@gmail.com>
2025-01-24 15:45:02 -0300

    PLG

--- plugins/Buscador_tweetposts.js
+++ plugins/Buscador_tweetposts.js
@@ -20,10 +20,10 @@ let resultsToDisplay = json.slice(0, 7);
 let mini = [];
 for (let res of resultsToDisplay) {
 
-let txt =  `✧ *User:* ${res.user}\n`
-    txt += `✧ *Publicacion:* ${res.post}\n`
-    txt += `✧ *Perfil:* ${res.profile}\n`
-    txt += `✧ *Link:* ${res.user_link}\n`
+let txt =  `✨ *User:* ${res.user}\n`
+    txt += `✨ *Publicacion:* ${res.post}\n`
+    txt += `✨ *Perfil:* ${res.profile}\n`
+    txt += `✨ *Link:* ${res.user_link}\n`
 
 mini.push({
 body: proto.Message.InteractiveMessage.Body.create({text: null}),
