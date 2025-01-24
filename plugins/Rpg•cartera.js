3112d1e8636b1d30abc4205309e70be9191c1857
Ian.xyz <taiwam34@gmail.com>
2025-01-24 17:42:20 -0300

    PLG

--- plugins/Rpg•cartera.js
+++ plugins/Rpg•cartera.js
@@ -2,13 +2,13 @@ let handler = async (m, {conn, usedPrefix}) => {
 	let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
 	if (who == conn.user.jid) return error 
 	const name = conn.getName(who);
-	let txt = `*✧ Balance de ${name}*
+	let txt = `*🌼 Balance de ${name}*
 	
 		➺ *${currency}* : ${global.db.data.users[who].yenes} 
 		➺ *Banco* : ${global.db.data.users[who].bank} 
 		➺ *Experiencia* : ${global.db.data.users[who].exp}
 	
-	> Para proteger tus *${currency}* en el banco use */depositar*`
+	> Para proteger tus *${currency}* en el banco use *.depositar*`
 	await conn.reply(m.chat, txt, m, )
 	}
 	
