b15203a9d0bbe702e811324c303b1d79811eceab
Ian.xyz <taiwam34@gmail.com>
2025-01-24 21:34:33 -0300

    PLG

--- plugins/Herramientas•hd.js
+++ plugins/Herramientas•hd.js
@@ -16,6 +16,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 handler.help = ["remini", "hd", "enhance"];
 handler.tags = ["ai", "tools"];
 handler.command = ["remini", "hd", "enhance"];
+handler.register = true
 export default handler;
 
 async function remini(imageData, operation) {
